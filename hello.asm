a3df3e2eab366ca43325d422799b78f1c047c596
animidox <dhanujadisanayke@gmail.com>
2025-01-06 21:26:43 +0530

    Half complete code

--- hello.asm
+++ hello.asm
@@ -1,16 +1,8 @@
 section .data
-    message db 'Hello, World!', 0  ; The message to print
+    message db "Hello, World", 0xA
+    length equ $ - message 
 
-section .text
-    global _start                 ; Entry point for the program
+section .text 
+    global _start
 
 _start:
-    mov eax, 4                    ; Syscall number for sys_write
-    mov ebx, 1                    ; File descriptor (stdout)
-    mov ecx, message              ; Address of the message
-    mov edx, 13                   ; Length of the message
-    int 0x80                      ; Make the system call
-
-    mov eax, 1                    ; Syscall number for sys_exit
-    xor ebx, ebx                  ; Exit code 0
-    int 0x80                      ; Make the system call
